82903a7fb48e3ec56bd4a859baac3ffb0899cce8
Gramzes <gramzin4666@gmail.com>
2023-02-12 14:10:11 +0300

    Добавлено меню

--- app/src/main/java/com/gramzin/androidquiz/MainActivity.kt
+++ app/src/main/java/com/gramzin/androidquiz/MainActivity.kt
@@ -2,15 +2,34 @@ package com.gramzin.androidquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.gramzin.androidquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        val navHostFragment = binding.fragmentContainerView.getFragment<NavHostFragment>()
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawer)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container_view)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/gramzin/androidquiz/RulesFragment.kt
@@ -0,0 +1,5 @@
+package com.gramzin.androidquiz
+
+import androidx.fragment.app.Fragment
+
+class RulesFragment : Fragment(R.layout.fragment_rules)
