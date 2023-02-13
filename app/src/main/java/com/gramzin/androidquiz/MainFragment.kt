2271179fdcbbcea3579adce0b796159d0b60177d
Gramzes <gramzin4666@gmail.com>
2023-02-13 17:53:38 +0300

    Добавлена анимация

--- app/src/main/java/com/gramzin/androidquiz/MainFragment.kt
+++ app/src/main/java/com/gramzin/androidquiz/MainFragment.kt
@@ -1,10 +1,13 @@
 package com.gramzin.androidquiz
 
+import android.content.res.ColorStateList
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.navigation.fragment.findNavController
 import com.gramzin.androidquiz.databinding.ActivityMainBinding
 import com.gramzin.androidquiz.databinding.FragmentMainBinding
@@ -25,6 +28,18 @@ class MainFragment : Fragment() {
 
         binding.startGameBtn.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_questionFragment)
+        }
+
+        binding.text.apply {
+            setBackgroundResource(R.drawable.back_anim)
+            val back = (background as AnimationDrawable)
+            back.setEnterFadeDuration(2000)
+            back.setExitFadeDuration(2000)
+            back.start()
+        }
+
+        AnimationUtils.loadAnimation(context, R.anim.btn_scale).also {
+            binding.startGameBtn.startAnimation(it)
         }
     }
 
